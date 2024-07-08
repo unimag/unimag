eaca437d82f3d536a8a7d5b7a4eae225a9324820
raman.shastapalau <raman.shastapalau@aventusit.eu>
2024-07-08 10:04:14 +0300

    feat: add gatsby engine

---
+++ src/website-config.ts
@@ -0,0 +1,75 @@
+export type WebsiteConfig = {
+  title: string;
+  description: string;
+  coverImage?: string;
+  logo: string;
+  /**
+   * Specifying a valid BCP 47 language helps screen readers announce text properly.
+   * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
+   */
+  lang: string;
+  /**
+   * blog full path, no ending slash!
+   */
+  siteUrl: string;
+  /**
+   * full url, no username
+   */
+  facebook?: string;
+  /**
+   * full url, no username
+   */
+  twitter?: string;
+  /**
+   * hide or show all email subscribe boxes
+   */
+  showSubscribe: boolean;
+  /**
+   * create a list on mailchimp and then create an embeddable signup form. this is the form action
+   */
+  mailchimpAction?: string;
+  /**
+   * this is the hidden input field name
+   */
+  mailchimpName?: string;
+  /**
+   * name and id of the mailchimp email field
+   */
+  mailchimpEmailFieldName?: string;
+  /**
+  /**
+   * Meta tag for Google Webmaster Tools
+   */
+  googleSiteVerification?: string;
+  /**
+  /**
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
+  /**
+   * Shows all post tags in main index view and post view if true
+   * Otherwise only shows first (primary) tag
+   */
+  showAllTags: boolean;
+};
+
+const config: WebsiteConfig = {
+  title: 'Unimag',
+  description: 'Блог с ИКС 11000 - всё для продвижения вашего сайта',
+  coverImage: 'img/main-bg.jpg',
+  logo: 'img/logo-unimag.png',
+  lang: 'en',
+  siteUrl: 'https://unimag.github.io/',
+  facebook: 'https://www.facebook.com/unimag',
+  twitter: 'https://twitter.com/unimag',
+  showSubscribe: true,
+  mailchimpAction:
+    'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  mailchimpEmailFieldName: 'MERGE0',
+  googleSiteVerification: 'GoogleCode',
+  footer: 'Сделано с любовью',
+  showAllTags: true,
+};
+
+export default config;
